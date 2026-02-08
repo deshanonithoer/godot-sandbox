7eb5fae183e330dea24982e9927724f929627987
Shano Nithoer <nithoer@d-tt.nl>
2026-02-08 21:07:31 +0100

    Some small additional changes

--- game.gd
+++ game.gd
@@ -3,9 +3,9 @@ extends Node2D
 #region Signal Connections
 func _on_menu_host_game(nick_name: String) -> void:
 	NetworkManager.host_game(nick_name)
-	%Menu.hide()
+	%MultiplayerMenu.hide()
 	
 func _on_menu_join_game(address: String, port: String, nick_name: String) -> void:
 	NetworkManager.join_game(address, port, nick_name)
-	%Menu.hide()
+	%MultiplayerMenu.hide()
 #endregion
